13d8a58d752fe148246b9e4c7b2db4eb6193e4ec
Dmytro Minochkin <dmytro.minochkin@gmail.com>
2023-09-02 13:04:04 +0300

    update k3s and ubuntu version (#20)

--- Terraform/GCP/cluster/nodes/data.tf
+++ Terraform/GCP/cluster/nodes/data.tf
@@ -1,5 +1,5 @@
 data "google_compute_image" "ubuntu" {
-  name = "ubuntu-2204-jammy-v20230429"
+  name = "ubuntu-2204-jammy-v20230829"
   # family = "ubuntu-2204-lts"
   project = "ubuntu-os-cloud"
 }
