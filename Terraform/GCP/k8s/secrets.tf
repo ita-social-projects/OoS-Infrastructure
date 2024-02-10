75310298d5bb168573bea897182e7c227508e491
sergiiglad <sergii.gladchenko@gmail.com>
2024-02-10 19:18:36 +0200

    del secret ilm-metricbeat

--- Terraform/GCP/k8s/secrets.tf
+++ Terraform/GCP/k8s/secrets.tf
@@ -178,11 +178,3 @@ resource "kubernetes_secret" "dns_gcp_credentials" {
   ]
 }
 
-resource "kubernetes_secret" "metricbeat-ilm-policy" {
-  metadata {
-    name      = "metricbeat-ilm-policy"
-  }
-  data = {
-    "metricbeat-ilm-policy.json" = file("${path.module}/config/metricbeat-ilm-policy.json")
-  }
-}
