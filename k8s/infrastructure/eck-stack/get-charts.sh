3e320eea3893c3dab7bc654ba04557457a511afb
sergiiglad <sergii.gladchenko@gmail.com>
2024-07-20 10:04:54 +0300

    upgarde elastic stack to v8.14.3

--- k8s/infrastructure/eck-stack/get-charts.sh
+++ k8s/infrastructure/eck-stack/get-charts.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 
 CHART_REPO=${CHART_REPO:-"elastic"}
 CHART_REPO_URL=${CHART_REPO_URL:-"https://helm.elastic.co"}
-CHART_ECK_VER=${CHART_VERSION:-"0.8.0"}
+CHART_ECK_VER=${CHART_VERSION:-"0.11.0"}
 
 echo "Adding ECK Helm chart"
 helm repo add "${CHART_REPO}" "${CHART_REPO_URL}"
@@ -14,3 +14,5 @@ helm repo update
 cd charts
 helm pull "${CHART_REPO}/eck-elasticsearch" --version "${CHART_ECK_VER}" --untar=false
 helm pull "${CHART_REPO}/eck-kibana" --version "${CHART_ECK_VER}" --untar=false
+helm pull "${CHART_REPO}/eck-beats" --version "${CHART_ECK_VER}" --untar=false
+helm pull "${CHART_REPO}/eck-apm-server" --version "${CHART_ECK_VER}" --untar=false
