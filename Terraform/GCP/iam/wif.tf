76b3db387d56b1b1de2e46c38b85c98545bfb03f
sergiiglad <sergii.gladchenko@gmail.com>
2025-01-15 14:48:25 +0200

    add wi pool and provider

---
+++ Terraform/GCP/iam/wif.tf
@@ -0,0 +1,23 @@
+# Configure Workload Identity Federation with K3s
+
+resource "google_iam_workload_identity_pool" "k3s_pool" {
+  project = var.project
+  workload_identity_pool_id = "k3s-cluster"
+  display_name              = "K3s Cluster"
+  description               = "Identity pool for k3s"
+}
+
+resource "google_iam_workload_identity_pool_provider" "onprem-cluster" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.k3s_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "k3s-prvdr"
+  display_name                       = "K3s Kubernetes Cluster"
+  oidc {
+    issuer_uri = "https://k8s.oos.dmytrominochkin.cloud:6443"
+    allowed_audiences = ["sts.googleapis.com"]
+  }
+  attribute_mapping = {
+    "google.subject" = "assertion.sub"
+    "attribute.kubernetes_namespace" = "assertion[\"kubernetes.io\"][\"namespace\"]"
+  }
+  attribute_condition = "attribute.kubernetes_namespace==\"csi\""
+}
