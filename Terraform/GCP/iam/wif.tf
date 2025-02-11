89740ec84ce19425612d74874aa7d5a5ecbaa704
sergiiglad <sergii.gladchenko@gmail.com>
2025-02-11 11:28:30 +0200

    add sa annotation

--- Terraform/GCP/iam/wif.tf
+++ Terraform/GCP/iam/wif.tf
@@ -23,8 +23,9 @@ resource "google_iam_workload_identity_pool_provider" "k3s_provider" {
   attribute_mapping = {
     "google.subject"                 = "assertion.sub"
     "attribute.kubernetes_namespace" = "assertion[\"kubernetes.io\"][\"namespace\"]"
+    "attribute.service_account_name" = "assertion[\"kubernetes.io\"][\"serviceaccount\"][\"name\"]"
   }
-  attribute_condition = "attribute.kubernetes_namespace==\"csi\""
+  attribute_condition = "attribute.kubernetes_namespace==\"default\""
 }
 
 resource "google_service_account" "secret_reader" {
