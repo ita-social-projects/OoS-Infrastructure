faff00c36e27817a08d38f225e11be5118821f5f
Dmytro M <dmytro.minochkin@gmail.com>
2025-02-21 20:51:34 +0200

    update jobs and Vector parsing

--- Terraform/GCP/k8s/csi_secret_driver.tf
+++ Terraform/GCP/k8s/csi_secret_driver.tf
@@ -14,13 +14,13 @@ resource "helm_release" "secrets_store_csi_driver" {
 
 resource "helm_release" "secret_store_csi_prv_gcp" {
   name             = "secret-store-csi-driver-provider-gcp"
-  chart            = "../../k8s/infrastructure/charts/secrets-store-csi-driver-provider-gcp-1.7.0.tgz"
+  chart            = "../../k8s/infrastructure/secrets-store-csi-driver-provider-gcp-1.7.0.tgz"
   create_namespace = true
   wait             = true
   wait_for_jobs    = true
 
   values = [
-    templatefile("${path.module}/values/secret-csi-prv-gcp.yaml",{
+    templatefile("${path.module}/values/secret-csi-prv-gcp.yaml", {
       GCP_PROJECT_ID     = var.project,
       GCP_KSA_TOKEN_PATH = var.wif_credentials.gcp_ksa_token_path,
       GSP_KSA_FILE       = var.wif_credentials.gsp_ksa_file,
@@ -36,12 +36,12 @@ resource "kubernetes_config_map" "wif_credentials" {
   }
 
   data = {
-    config =  templatefile("${path.module}/config/wif_k3s/${var.wif_credentials.gsp_ksa_file}", {
-      WIF_PROVIDER_NAME     = var.wif_provider_name,
-      SECRET_READER_SA      = var.secret_reader_sa_email,
-      WIF_K3S_TOKEN_PATH    = var.wif_credentials.gcp_ksa_token_path,
+    config = templatefile("${path.module}/config/wif_k3s/${var.wif_credentials.gsp_ksa_file}", {
+      WIF_PROVIDER_NAME  = var.wif_provider_name,
+      SECRET_READER_SA   = var.secret_reader_sa_email,
+      WIF_K3S_TOKEN_PATH = var.wif_credentials.gcp_ksa_token_path,
     })
-   }
+  }
 }
 
 resource "kubectl_manifest" "secret_csi" {
