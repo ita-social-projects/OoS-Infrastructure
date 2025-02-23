a50a1b908993309103de7162d146ee065214070a
Dmytro M <dmytro.minochkin@gmail.com>
2025-02-23 18:24:44 +0200

    add encryption deploy

--- Terraform/GCP/build/main.tf
+++ Terraform/GCP/build/main.tf
@@ -42,7 +42,7 @@ resource "google_cloudbuild_trigger" "app_deploy" {
     _SERVICE_NAME    = "webapi"
     _REGION          = var.region
     _VALUES_PATH     = "./k8s/infrastructure/webapi.yaml"
-    _KSA_ANNOTATIONS = "serviceAccount.annotations.iam\\.gke\\.io/gcp-service-account=${var.secret_reader_sa_email}"
+    _KSA_ANNOTATIONS = local.ksa_annotations
   }
 
   source_to_build {
@@ -58,6 +58,40 @@ resource "google_cloudbuild_trigger" "app_deploy" {
     repo_type = "GITHUB"
   }
   filter          = "_ACTION.matches(\"INSERT\") && _IMAGE_TAG.matches(\"^.*oos-api:.*$\")"
+  service_account = var.build_sa_id
+}
+
+resource "google_cloudbuild_trigger" "encription_deploy" {
+  location = var.region
+  name     = "backend-encription-deploy"
+  pubsub_config {
+    topic = google_pubsub_topic.gcr.id
+  }
+
+  substitutions = {
+    _KUBE_CONFIG     = var.kube_secret
+    _POOL            = google_cloudbuild_worker_pool.pool.id
+    _ACTION          = "$(body.message.data.action)"
+    _IMAGE_TAG       = "$(body.message.data.tag)"
+    _SERVICE_NAME    = "encription"
+    _REGION          = var.region
+    _VALUES_PATH     = "./k8s/infrastructure/encription.yaml"
+    _KSA_ANNOTATIONS = local.ksa_annotations
+  }
+
+  source_to_build {
+    uri       = "https://github.com/ita-social-projects/OoS-Infrastructure"
+    ref       = "refs/heads/main"
+    repo_type = "GITHUB"
+  }
+
+  git_file_source {
+    path      = "cloudbuild-deploy.yml"
+    uri       = "https://github.com/ita-social-projects/OoS-Infrastructure"
+    revision  = "refs/heads/main"
+    repo_type = "GITHUB"
+  }
+  filter          = "_ACTION.matches(\"INSERT\") && _IMAGE_TAG.matches(\"^.*oos-encr:.*$\")"
   service_account = var.build_sa_id
 }
 
